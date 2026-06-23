8781c266e6741974aff0faa183a747e8aff82e1f
Raspbsb <unionbjh@naver.com>
2026-06-23 18:11:20 +0900

    feat: 데이터 렌더링 및 스타일

--- React-mission/src/App.jsx
+++ React-mission/src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
-import { useState } from "react";
-import CardList from "./components/Card.jsx";
+import RenderCards from "./components/CardList.jsx";
+
+// 상품 데이터
+const products = [
+  { id: 1, name: "무선 키보드", price: 39000, brand: "EZTECH" },
+  { id: 2, name: "무선 마우스", price: 29000, brand: "EZTECH" },
+  { id: 3, name: "모니터", price: 199000, brand: "EZTECH" },
+  { id: 4, name: "모니터", price: 199000, brand: "EZTECH" },
+  { id: 5, name: "모니터", price: 199000, brand: "EZTECH" },
+  { id: 6, name: "모니터", price: 199000, brand: "EZTECH" },
+  { id: 7, name: "모니터", price: 199000, brand: "EZTECH" },
+  { id: 8, name: "모니터", price: 199000, brand: "EZTECH" },
+  { id: 9, name: "모니터", price: 199000, brand: "EZTECH" },
+];
 
 function App() {
-  return <CardList />;
+  return (
+    <div className="wrapper">
+      {/* 가져온 RenderCards 함수에 매개변수 data의 인수로 상품 데이터 전체를 넣고 돌려 */}
+      {/* 상품 카드 데이터를 받아서 순회 렌더링 후 반환하는 함수 */}
+      <RenderCards data={products} />
+    </div>
+  );
 }
 
 export default App;
---
+++ React-mission/src/components/CardList.jsx
@@ -0,0 +1,39 @@
+// css모듈을 가져와서 이 파일에만 적용
+import styles from "./Card.module.css";
+
+// id, name, price, brand 객체 배열인 상품 카드 데이터를 받아서 순회 렌더링 후 반환하는 함수
+function RenderCards({ data }) {
+  console.log(data); // data 전체 (products = [ {}, {}, {}, ])
+
+  // 받은 데이터에서 각 배열요소인 객체의 각 정보를 받아서 순회하고 cards에 저장
+  // 객체 구조 분해 할당 활용했습니다.
+  const cards = data.map(({ id, name, price, brand }) => {
+    console.log({ id, name, price, brand }); // {id: 0, name: "", price: 0, brand: ""}
+
+    // 이 형식으로 데이터 저장
+    return (
+      <li key={id}>
+        <div className={styles.card}>
+          <div className={styles.productThumb}></div>
+          <div className={styles.contents}>
+            <div className={styles.cardHeader}>
+              <h2>{name}</h2>
+              <p className={styles.brand}>{brand}</p>
+            </div>
+            <p className={styles.price}>{price}원</p>
+          </div>
+        </div>{" "}
+      </li>
+    );
+  });
+
+  // 최종적으로 섹션-ul내부에 출력
+  return (
+    <section className={styles.productList}>
+      <ul className={styles.cardGrid}>{cards}</ul>
+    </section>
+  );
+}
+
+// 내보내기
+export default RenderCards;
